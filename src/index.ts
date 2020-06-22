06fb3083cc16270967588c0a153e8bc470c07e3a
mramoss <moi6ramoss@gmail.com>
2020-06-22 15:15:07 -0400

    Operador map

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,17 @@
-import { from, of } from "rxjs";
+import { fromEvent, range } from "rxjs";
 
-import { async } from "rxjs/internal/scheduler/async";
+import{map} from 'rxjs/operators'
 
-const observer ={
-    next: val => console.log('next:', val),
-    complete: () => console.log('complete')  
-};
-
-/** 
-* of = toma argumentos y genera una secuencia
-* from = array, promesa, iterable, observable
+/* 
+range(1,5).pipe(
+    map<number,string>(val =>(val*10).toString())
+).subscribe( console.log); 
 */
 
-// const source$ = from([1,2,3,4,5]);
-// const source$ = of(... [1,2,3,4,5]);
+const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup');
+const keyupCode$ = keyup$.pipe(
+    map(event => event.code)
+)
 
-// const source$ = from('Fernando');
 
-const source$ = from( fetch('https://api.github.com/users/moi6rs'));
-
-// source$.subscribe(observer);
-
-const miGenerador = function*(){
-    yield 1;
-    yield 2;
-    yield 3;
-    yield 4;
-    yield 5;
-}
-
-const miIterable = miGenerador();
-
-/*
-for(let id of miIterable){
-    console.log(id);
-    
-}*/
-
-from( miIterable).subscribe(observer);
-
-/* source$.subscribe( async (resp) =>{
-    console.log(resp.url);
-    const dataResp = await resp.json();
-    console.log(dataResp);
-}) */
+keyupCode$.subscribe(code => console.log('map', code));
